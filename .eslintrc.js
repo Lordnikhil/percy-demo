370aa6b00adea84adaf5edde1a8b58b83350f22c
Nikhil <spider4597@gmail.com>
2022-12-23 17:08:21 +0530

    percy init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+export default {
+  "plugins": [
+    "react-percy"
+  ],
+  "env": {
+    "react-percy/globals": true
+  }
+  // "overrides": [
+  //   {
+  //     "files": [
+  //       "**/*.percy.{js,jsx}"
+  //     ],
+  //     "env": {
+  //       "react-percy/globals": true
+  //     }
+  //   }
+  // ]
+}
---
+++ src/Button.percy.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from './Button';
+
+percySnapshot('Button', () => {
+  return <Button>My Button</Button>
+});
